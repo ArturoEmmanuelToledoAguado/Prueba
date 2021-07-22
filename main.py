43ce7ca8c963eacf6041b4150dd3729fa4186342
ThunderGer <Jaames11@hotmail.com>
2021-07-21 20:57:21 -0500

    update file main.py add function number

--- main.py
+++ main.py
@@ -7,7 +7,12 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print_d()
 
+def print_d():
+    a = 5
+    b = 7
+    print(str(b/a))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
